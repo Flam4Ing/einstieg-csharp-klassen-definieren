139a4338f840717cc987103b6d5d340d65c9cf6c
Jan Suchotzki <jan@lernmoment.de>
2016-01-15 16:10:27 +0100

    Erstellt Objekte von der Klasse Auto und verwendet sie.

--- KlassenDefinieren/Program.cs
+++ KlassenDefinieren/Program.cs
@@ -7,6 +7,17 @@ namespace KlassenDefinieren
     {
         static void Main(string[] args)
         {
+            Auto meinAuto = new Auto();
+            Auto deinAuto = new Auto();
+
+            meinAuto.alterInJahren = 13;
+            meinAuto.marke = "Audi";
+
+            deinAuto.alterInJahren = 1;
+            deinAuto.marke = "Porsche";
+
+            meinAuto.BeschleunigeAuf100KmH();
+            deinAuto.BeschleunigeAuf100KmH();
         }
     }
 }
