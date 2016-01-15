77ccf38fb3ff5a2b07cd5563610a04ea50def064
Jan Suchotzki <jan@lernmoment.de>
2016-01-15 15:14:03 +0100

    Initialer Commit

---
+++ KlassenDefinieren/Auto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KlassenDefinieren
+{
+    public class Auto
+    {
+        public string marke;
+        public int alterInJahren;
+
+        public void BeschleunigeAuf100KmH()
+        {
+            if (IstSchnell())
+            {
+                Console.WriteLine("Habe schnell beschleunigt! Yeah!");
+            }
+            else
+            {
+                Console.WriteLine("Habe normal beschleunigt!");
+            }
+        }
+
+        private bool IstSchnell()
+        {
+            if (marke == "Porsche")
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ KlassenDefinieren/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace KlassenDefinieren
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
