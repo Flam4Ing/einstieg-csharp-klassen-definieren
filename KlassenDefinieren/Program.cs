454e23ac99d3816cbe61ccce7280302c062d9b25
Jan Suchotzki <jan@lernmoment.de>
2016-01-16 16:29:35 +0100

    Fügt einen Kontruktor mit Parameter Marke hinzu.

--- KlassenDefinieren/Program.cs
+++ KlassenDefinieren/Program.cs
@@ -6,14 +6,11 @@ namespace KlassenDefinieren
     {
         static void Main(string[] args)
         {
-            Auto meinAuto = new Auto();
-            Auto deinAuto = new Auto();
+            Auto meinAuto = new Auto("Audi");
+            Auto deinAuto = new Auto("Porsche");
 
             meinAuto.alterInJahren = 13;
-            meinAuto.marke = "Audi";
-
             deinAuto.alterInJahren = 1;
-            deinAuto.marke = "Porsche";
 
             Console.WriteLine("Mein Auto fährt aktuell {0} km/h.", meinAuto.SagAktuelleGeschwindigkeit());
             Console.WriteLine("Dein Auto fährt aktuell {0} km/h.", deinAuto.SagAktuelleGeschwindigkeit());
